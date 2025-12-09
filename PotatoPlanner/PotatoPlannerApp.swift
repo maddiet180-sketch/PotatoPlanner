fc5f9330145e8c1a1a7b1a5af8669c26232badc9
Madeline Moody <maddie.t180@gmail.com>
2025-12-09 12:00:58 -0500

    Initial Commit

---
+++ PotatoPlanner/PotatoPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotatoPlannerApp.swift
+//  PotatoPlanner
+//
+//  Created by Maddie Moody on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PotatoPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
